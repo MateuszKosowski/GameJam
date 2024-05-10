4bf622c624bbdad33f5f786fbc0a55adad3da458
MateuszKosowski <mateuszkosowski03@gmail.com>
2024-05-10 20:59:42 +0200

    Shooting rdy

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
-	
+# Shooting
+var bullet_scane: PackedScene = preload("res://scenes/bullet/bullet.tscn")
 func _on_player_shoot(bulletPosition, bulletDirection):
-	print(bulletPosition)
-	print(bulletDirection)
+	var bullet = bullet_scane.instantiate() as Area2D
+	bullet.position = bulletPosition
+	bullet.direction = bulletDirection
+	$Bullets.add_child(bullet)
