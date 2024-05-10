a4f9b6781ac826c94f00bcd45a24b59c5f56ee0a
Eryk Dudkiewicz <128227580+1iekki@users.noreply.github.com>
2024-05-11 00:37:23 +0200

    gimpur strzela

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -14,3 +14,13 @@ func _on_remove_bullet_timeout():
 	for n in $Bullets.get_children():
 		$Bullets.remove_child(n)
 		n.queue_free()
+
+
+func _on_enemy_shoot(bulletPosition, bulletDirection):
+	var bullet = bullet_scane.instantiate() as Area2D
+	bullet.position = bulletPosition
+	bullet.direction = bulletDirection
+	bullet.target = "player"
+	$Bullets.add_child(bullet)
+	$RemoveBullet.start()
+	pass # Replace with function body.
