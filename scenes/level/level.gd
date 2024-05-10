bd4e757a62dc3a16bbed48e27dee96fea6b116c0
MateuszKosowski <mateuszkosowski03@gmail.com>
2024-05-10 22:34:14 +0200

    Add removing magicBall

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -7,3 +7,10 @@ func _on_player_shoot(bulletPosition, bulletDirection):
 	bullet.position = bulletPosition
 	bullet.direction = bulletDirection
 	$Bullets.add_child(bullet)
+	$RemoveBullet.start()
+	
+
+func _on_remove_bullet_timeout():
+	for n in $Bullets.get_children():
+		$Bullets.remove_child(n)
+		n.queue_free()
