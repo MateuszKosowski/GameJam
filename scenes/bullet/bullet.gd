9f9453449f0187f551399019c4cde762729fc646
VirtuaIL <121065635+VirtuaIL@users.noreply.github.com>
2024-05-11 08:53:07 +0200

    skip cutscene

--- scenes/bullet/bullet.gd
+++ scenes/bullet/bullet.gd
@@ -6,8 +6,11 @@ var target = "enemy"
 
 func _process(delta):
 	position += direction * speed * delta
-
+	
+	
 func _on_body_entered(body):
 	if body.has_method("handle_hit") and body.is_in_group(target):
 		body.handle_hit()
+		queue_free()
+	queue_free()
 	pass # Replace with function body.
