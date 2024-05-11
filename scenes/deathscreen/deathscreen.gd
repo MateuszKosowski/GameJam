586782e8d47b44a1494578f372912f023a945ba5
VirtuaIL <121065635+VirtuaIL@users.noreply.github.com>
2024-05-11 09:30:37 +0200

    death screen

--- scenes/deathscreen/deathscreen.gd
+++ scenes/deathscreen/deathscreen.gd
@@ -1,4 +1,10 @@
 extends Node2D
 
-func _ready():
-	$AnimationPlayer.play("fadein")
+
+func _on_button_no_pressed():
+	#print("no")
+	get_tree().change_scene_to_file("res://scenes/level/level.tscn")
+
+func _on_button_yes_pressed():
+	#print("yes")
+	get_tree().quit()
