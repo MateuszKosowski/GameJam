4a3cccf4e753ae97f94b662fd126440eaddd6f32
Eryk Dudkiewicz <128227580+1iekki@users.noreply.github.com>
2024-05-11 14:44:02 +0200

    Revert "Merge branch 'hp'"
    This reverts commit 47533e18c8d3b52f45a5e4ea6fbc83227d428fc5, reversing
    changes made to fa10f98328f7a7123da1a5078d90c1726fff16c2.

--- scenes/lantern/lantern.gd
+++ scenes/lantern/lantern.gd
@@ -2,20 +2,13 @@ extends Node2D
 
 var active:bool = true
 var playerin: bool = false
-var player = null
-
-signal playerInLight(playr)
-signal playerOutLight(playr)
 
 func _process(delta):
 	if active:
 		$PointLight2D.set_enabled(true)
 		if playerin and $TimerOn.is_stopped():
 			$TimerOn.start()
-			playerInLight.emit(player)
 	if !active:
-		if playerin:
-			playerOutLight.emit(player)
 		$PointLight2D.set_enabled(false)
 
 func _ready():
@@ -24,7 +17,6 @@ func _ready():
 
 func _on_player_detect_body_entered(body):
 	if body.is_in_group("player"):
-		player = body
 		playerin = true
 
 func _on_timer_off_timeout():
@@ -36,5 +28,4 @@ func _on_timer_on_timeout():
 
 func _on_player_detect_body_exited(body):
 	if(body.is_in_group("player")):
-		playerOutLight.emit(player)
 		playerin = false
