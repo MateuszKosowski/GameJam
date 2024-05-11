30ed86f6b889f2376fb7f0a8401c3861bb564829
VirtuaIL <121065635+VirtuaIL@users.noreply.github.com>
2024-05-11 04:21:16 +0200

    charging_lights

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,34 +2,25 @@ extends CharacterBody2D
 
 var canShoot: bool = true
 var shootDirection: Vector2
-var looking_direction: bool = true
+#var looking_direction: bool = true
+var can_charge: bool = true
+
 
 signal shoot(bulletPosition, bulletDirection)
 
 func _process(delta):
 	
+	
 	#Player movement
 	var direction = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
 	position += direction * 200 * delta
 	
+	move_and_slide()
 	#Player flip horizontally
 	if direction.x < 0:
 		$AnimatedSprite2D.play("facing_left")
-		#looking_direction = true
 	if direction.x > 0:
 		$AnimatedSprite2D.play("default")
-		#looking_direction = false
-	#$AnimatedSprite2D.flip_h=looking_direction
-	
-	#if Input.is_action_pressed("shootRight") and canShoot and Input.is_action_pressed("moveLeft"):
-		#shootDirection = Vector2.RIGHT
-		#print("komplikacja")
-		
-		#$AnimatedSprite2D.play("default")
-		#$AnimatedSprite2D.animation_looped
-	
-	
-	
 	
 	#Player shooting
 	if Input.is_action_pressed("shootUp") and canShoot:
@@ -37,36 +28,34 @@ func _process(delta):
 	if Input.is_action_pressed("shootDown") and canShoot:
 		shootDirection = Vector2.DOWN
 	if Input.is_action_pressed("shootLeft") and canShoot:
-		#if Input.is_action_pressed("moveRight"):
-			#$ShootingInOposingDirectionTImer.start()
-			#$AnimatedSprite2D.play("facing_left")
-			#if $AnimatedSprite2D. != "facing_left":
-				#$AnimatedSprite2D.play("facing_left")
-			#else:
-				#$AnimatedSprite2D.stop()
-				
-			#$AnimatedSprite2D.animation_looped
-			#$AnimatedSprite2D.loop=true
-			#$ShootingInOposingDirectionTImer.is_stopped()
-			#print("biegnie w prawo, strzela w lewo")
-		#else:
-			#$AnimatedSprite2D.stop()
-			#$AnimatedSprite2D.flip_h= true
 		shootDirection = Vector2.LEFT
 	if Input.is_action_pressed("shootRight") and canShoot:
-		#if Input.is_action_pressed("moveLeft"):
-			#$AnimatedSprite2D.play("default")
-		#if looking_direction == true:
-			#print("biegnie w lewo, strzela w prawo")
-			#$AnimatedSprite2D.flip_h= true
 		shootDirection = Vector2.RIGHT
-		
 	if Input.is_action_pressed("shootGeneral") and canShoot:
 		var selectedShootPos = $ShootPos.get_child(0)
 		canShoot = false
 		$ShootReloadTimer.start()
 		shoot.emit(selectedShootPos.global_position, shootDirection)
+			
+	if Input.is_action_pressed("charge") and can_charge:
+		print("charge")
+		can_charge=false
+		$ChargeTimerCooldown.start()
+		$ChargingTime.start()
+		$".".set_process(false)
+		
+
+
 
 # Reload canShoot
 func _on_shoot_reload_timer_timeout():
 	canShoot = true
+
+func _on_charge_timer_cooldown_timeout():
+	can_charge=true
+	print("Charge cooldown over")
+	
+func _on_charging_time_timeout():
+	print("Charging Time over")
+	$".".set_process(true)
+	
