ddc2800709930afc924340aae81fd9ae738d117d
Eryk Dudkiewicz <128227580+1iekki@users.noreply.github.com>
2024-05-11 13:08:49 +0200

    a

--- scenes/mob_spawner/mob_spawner.gd
+++ scenes/mob_spawner/mob_spawner.gd
@@ -5,7 +5,8 @@ signal spawnMob(where, player)
 
 func _process(delta):
 	if $Timer.is_stopped():
-		$Timer.start()
+		if randi()%5 == 0:
+			$Timer.start()
 
 func _on_timer_timeout():
 	spawnMob.emit(self.position, player)
